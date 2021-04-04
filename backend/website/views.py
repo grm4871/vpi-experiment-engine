e9f4d0f32888e6e1360dfc5ebef5e59fe03f6300
Quinn Tucker <qxzcraft@gmail.com>
2021-04-03 20:58:19 -0400

    Add CSS and store form data in user's session

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, session
 
-from connection import get_connection
+# from connection import get_connection
 
 views = Blueprint('views', __name__)
 
@@ -13,21 +13,23 @@ def home():
     """
     return render_template("index.html")
 
+INITIAL_QUESTION_FIELDS = ['name', 'age', 'sex', 'color', 'lenses']
 
 # gets experiment
 @views.route("/experiment", methods=['POST', 'GET'])
 def experiment():
-    """
-    :return: experiment render template
-    """
+    if request.method == 'POST':
+        # store the form data in the user's session
+        form_data = request.form
+        for field in INITIAL_QUESTION_FIELDS:
+            session[field] = form_data[field]
 
-    if request.method == 'GET':
-        pass
-    if request.method == 'POST'
+        # show the experiment page
+        return render_template("experiment.html")
 
-        # get form data
+    elif request.method == 'GET':
+        # TODO: if the user hasn't done the initial questions,
+        # redirect them to the home page
 
-        # form_data = request.form
-        # age = form_data['age']
-
+        # show the experiment page
         return render_template("experiment.html")
