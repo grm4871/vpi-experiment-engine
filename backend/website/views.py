b7358eec0a5ef81cb25eda94c8047e42b41679a5
grm4871 <55329374+grm4871@users.noreply.github.com>
2021-04-04 20:36:15 -0400

    wrote route for saturation experiment

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, session
+import random as rand #paul
 
 # from connection import get_connection
 
@@ -33,3 +34,25 @@ def experiment():
 
         # show the experiment page
         return render_template("experiment.html")
+
+NUM_SAT_EXPERIMENTS = 12
+
+# gets sat_experiment
+@views.route("/satexp", methods=['POST', 'GET'])
+def satexp():
+    experiment_data = {}
+    experiment_data['num'] = NUM_SAT_EXPERIMENTS
+    if request.method == 'POST':
+        # do experiment RNG
+        for i in range(NUM_SAT_EXPERIMENTS):
+            original = 100
+            altered = rand.choice([50,75,125,150])  
+            experiment_data['A'].append(original)
+            experiment_data['B'].append(altered) 
+            if rand.getrandbits(1):
+                # swap
+                experiment_data['A'][i], experiment_data['B'][i] = experiment_data['B'][i], experiment_data['A'][i]
+        return render_template("satexp.html", experiment_data)
+    else:
+        pass
+        # todo go back home
