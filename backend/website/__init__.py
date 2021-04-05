68bf3b3cd5d6ec32a40cc597ce3d1b03c163c7c6
Quinn Tucker <qxzcraft@gmail.com>
2021-04-05 11:32:38 -0400

    Automatically generate and save secret_key

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from pathlib import Path
+import os
 
+
+SECRET_KEY_FILE = Path('secret_key')
 
 def create_app():
     app = Flask(__name__)
-    
+
+    try:
+        # try to load the secret key from the file
+        secret_key = SECRET_KEY_FILE.read_bytes()
+    except FileNotFoundError:
+        # if none exists, create a new secret key and save it
+        secret_key = os.urandom(16)
+        SECRET_KEY_FILE.write_bytes(secret_key)
+    app.secret_key = secret_key
+
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
