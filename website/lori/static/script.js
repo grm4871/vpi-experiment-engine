0586a27a4a85c851f8b1b38dd906808cd0fee89e
Quinn Tucker <qxzcraft@gmail.com>
2021-04-14 01:29:43 -0400

    Some minor improvements/fixes in Lori's experiment

--- website/lori/static/script.js
+++ website/lori/static/script.js
@@ -1,22 +1,69 @@
-const isExperimentPage = document.documentElement.classList.contains('experiment');
+const TIMER = 3;
+const MAX_STATES = 3;
+const NUM_TRIALS = 2;
 
-var timer = 6;
+var trial = 0;
+var timer = TIMER;
 var state = 0;
+var img = 1;
+var counting = true;
 
-countDown()
-console.log(timer);
+countDown(counting)
 
+function beginExperiment() {
+    document.getElementById('instructions').hidden = true;
+    document.getElementById('experiment').hidden = false;
+    counting = true;
+    img = 1;
+    state = 0;
+    timer = TIMER;
+    countDown(counting);
+}
+
+// Counts down. Responsible for state rules and displaying images.
 function countDown() {
     timer -= 1;
-    console.log(timer);
-    if (state == 0) {
-        document.getElementById('timer').innerHTML = `Your experiment will begin in ${timer}.`
-        
-        if(timer == 1) {
+    if (timer == 0) {
             state++;
-        }
+            displayImage(img);
+            timer = TIMER - 1;
+            img++;
     }
-    else if(state == 1) {
+
+    //End of first trial
+    if(state == MAX_STATES) {
+        trial++;
+        counting = false;
+        document.getElementById('experiment').hidden = true;
+        document.getElementById('test').hidden = false;
+        document.getElementById('slideshow').src = ``;
     }
-    window.setTimeout(countDown, 1000);
+
+    //End of final trial
+    if (state == MAX_STATES && trial == NUM_TRIALS) {
+        counting = false;
+        document.getElementById('instructions').hidden = true;
+        document.getElementById('test').hidden = false;
+        document.getElementById('cont_button').onclick = 'finish()';
+        document.getElementById('cont_button').value = "Submit";
+    }
+
+    else if (state == 0) {
+        document.getElementById('timer').innerHTML = `Your experiment will begin in ${timer}.`
+    }
+    else if(state >= 1) {
+        document.getElementById('timer').innerHTML = `Time left: ${timer}`
+    }
+    if (counting) {
+        window.setTimeout(countDown, 1000);
+    }
+}
+
+function displayImage(img) {
+    document.getElementById('slideshow').src = `static\\images\\_\\${img}_whitem.jpeg`;
+}
+
+function displayInstructions() {
+    document.getElementById('instructions').hidden = false;
+    document.getElementById('test').hidden = true;
 }
